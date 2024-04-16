9c3986272ac26f0d301868c65f42e54f18f80758
Phuykong Meng <phuykong.meng@temple.edu>
2024-04-16 13:10:59 -0400

    update

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS
-from yadda import getGPT, poop
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 import pickle
+import pandas as pd
+from surprise import Dataset, Reader
+from surprise.model_selection import train_test_split
 
 app = Flask(__name__)
 CORS(app)
 
 # Load pipeline and model using the binary files
 model = pickle.load(open('poopie_model.pkl', 'rb'))
+data = pd.read_csv('bathroom_data_set.csv')
+# Define the rating scale
+reader = Reader(rating_scale=(1, 5))
+
+# Load the dataset into Surprise format
+dataset = Dataset.load_from_df(data[['building', "Floor-Section", "rating" ]], reader)
+
+# Split the dataset into training and testing sets
+trainset, testset = train_test_split(dataset, test_size=0.4, random_state=42)
+
 
 
 @app.route('/startFunction')
 def success():
-   # answer = getGPT("smurf4934@gmail.com","123456!")
-   answer = getGPT()
-   return '%s' % (answer)
+   try:
+      all_bathrooms = dataset.build_full_trainset().build_anti_testset()
+      all_predictions = model.test(all_bathrooms)
+
+      # Rank bathrooms based on predicted ratings
+      ranked_bathrooms = sorted(all_predictions, key=lambda x: x.est, reverse=True)
+   except Exception as e:
+      print(e)
+      return (f"error: {e}", 500)
+
+   return (list(ranked_bathrooms[:10]), 200)
 
 @app.route('/poop')
 def poop():
-   # answer = getGPT("smurf4934@gmail.com","123456!")
-   answer = poop()
-   return '%s' % (answer)
+   return "poop"
 
 
 @app.route('/error')
